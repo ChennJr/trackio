372a234e7bca4d8d9e8ba6dc4b696c16278ff367
Jackson <jacksonchen64299@gmail.com>
2024-10-30 09:58:40 +0000

    Version 0.0.3

--- mvp/model.py
+++ mvp/model.py
@@ -2,11 +2,25 @@ import json
 import glob
 import time
 import pandas as pd
+import psutil
+import os
+
 
 class SpotifyDatasetProcessor:
     def __init__(self, directory):
         self.directory = sorted(glob.glob(directory + r"\*json"), key=len)
-    
+        self.csv_file = r"mvp\dataset.csv"
+
+    def create_empty_csv(self):
+        if not os.path.exists(self.csv_file):
+            columns = ['artist_name', 'track_uri', "artist_uri", 'track_name']
+            df = pd.DataFrame(columns=columns)
+            df.to_csv(self.csv_file, index=False)
+            print(f"{self.csv_file} created with columns: {', '.join(columns)}")
+
+        else:
+            print(f"{self.csv_file} already exists.")
+
     def get_all_tracks_info(self, file_path):
         track_data = []
 
@@ -21,37 +35,45 @@ class SpotifyDatasetProcessor:
     def clean_data(self):
         time_start = time.process_time()
         tracks_list = []
-        chunk_size = 2500000
+        self.create_empty_csv()
+
         for file in self.directory:
             track_data = self.get_all_tracks_info(file)
             tracks_list.extend(track_data)
 
-            if len(tracks_list) >= chunk_size:
+            if int(psutil.virtual_memory().available / 1000000000) <= 3:
                 df = pd.DataFrame(tracks_list)
-                df.drop(columns=["pos", "duration_ms", "album_name", "album_uri"], inplace=True)
-                df.drop_duplicates(subset=["track_uri"], ignore_index=True, inplace=True)
-                df.to_csv(r"mvp\dataset.csv", mode="a", index=False)
+                df.drop(columns=["pos", "duration_ms",
+                        "album_name", "album_uri"], inplace=True)
+                df.drop_duplicates(
+                    subset=["track_uri"], ignore_index=True, inplace=True)
+                df.to_csv(r"mvp\dataset.csv", mode="a", index=False, header=False)
                 tracks_list.clear()
-            
-            percentage_complete = (((int(self.directory.index(file) + 1)) / 1000) * 100)
+
+            percentage_complete = (
+                ((int(self.directory.index(file) + 1)) / 1000) * 100)
 
             print(f"{'{:.2f}'.format(percentage_complete)}% completed")
 
-        
         df = pd.DataFrame(tracks_list)
-        df.drop(columns=["pos", "duration_ms", "album_name", "album_uri"], inplace=True)
-        df.drop_duplicates(subset=["track_uri"], ignore_index=True, inplace=True)
-        df.to_csv(r"mvp\dataset.csv", mode="a", index=False)
+        df.drop(columns=["pos", "duration_ms",
+                "album_name", "album_uri"], inplace=True)
+        df.drop_duplicates(subset=["track_uri"],
+                           ignore_index=True, inplace=True)
+        df.to_csv(r"mvp\dataset.csv", mode="a", index=False, header=False)
 
         df = pd.read_csv(r"mvp\dataset.csv")
-        df.drop_duplicates(subset=["track_uri"], ignore_index=True, inplace=True)
+        df.drop_duplicates(subset=["track_uri"],
+                           ignore_index=True, inplace=True)
         df.to_csv(r"mvp\dataset.csv", mode="w", index=False)
 
         time_taken = time.process_time() - time_start
         print(f"Time taken: {time_taken} seconds")
 
 
-processor = SpotifyDatasetProcessor(directory=r"mvp\spotify_million_playlist_dataset\data")
+processor = SpotifyDatasetProcessor(
+    directory=r"mvp\spotify_million_playlist_dataset\data")
+
 processor.clean_data()
 df = pd.read_csv(r"mvp\dataset.csv")
 print(df)
